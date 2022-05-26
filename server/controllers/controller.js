09bb058dac9035436f534158a84222c94cdd6db7
unknown <lukasnesittka25@gmail.com>
2022-05-26 16:16:33 -0500

    put-complete

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,9 +1,24 @@
+let namesArr = []
+
 module.exports = {
     addName: (req,res) => {
-
+        console.log(req.params,req.body,req.query)
+        namesArr.push(req.body.name)
+        console.log(namesArr)
+        res.status(200).send(namesArr)
     },
     updateName: (req,res) => {
-
+        console.log(req.body)
+        const { original,changed } = req.body
+        namesArr = namesArr.map(el => {
+            if(el === original){
+                return el = changed
+            } else {
+                return el
+            }
+        })
+        console.log(namesArr)
+        res.status(200).send(namesArr)
     },
     deleteName: (req,res) => {
 
